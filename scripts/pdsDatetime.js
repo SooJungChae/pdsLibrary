ca528cc007bde8ed401996b679d3e2d954dce71b
SooJungChae <naanace@gmail.com>
2018-08-21 17:41:44 +0900

    Import pdsDatetime module

---
+++ scripts/pdsDatetime.js
@@ -0,0 +1,9 @@
+/*-----------------------------------
+    This is "pdsDatatime" library
+    Author : SooJung Chae
+    Developer Date : 2018-08-17
+ ----------------------------------*/
+
+export default () => {
+    console.log('datetime start');
+}
