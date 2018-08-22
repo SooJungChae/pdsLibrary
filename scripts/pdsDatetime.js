317887cfa2c9a4f072e83b537d74c4b244e32212
SooJungChae <naanace@gmail.com>
2018-08-22 10:09:11 +0900

    Edit babelrc

--- scripts/pdsDatetime.js
+++ scripts/pdsDatetime.js
@@ -3,7 +3,14 @@
     Author : SooJung Chae
     Developer Date : 2018-08-17
  ----------------------------------*/
+const defaults = () => {
+    styles: {
 
-export default () => {
-    console.log('datetime start');
+    },
+    
+};
+
+export default (obj, attr) => {
+    console.log(obj);
+    console.log(attr);
 }
