6f6b5243ef73f33c6ecd7776bd94f4c3d606c819
SooJungChae <naan_ace@naver.com>
2018-08-23 14:38:48 +0900

    Set babel settings and download babel-cli babel-preset-2015 babel-preset-env

--- pdsDatetime/scripts/pdsDatetime.js
+++ pdsDatetime/scripts/pdsDatetime.js
@@ -5,12 +5,18 @@
  ----------------------------------*/
 const defaults = () => {
     styles: {
-
-    },
+        width: 150
+    }
     
 };
 
+const initialize = (obj) => {
+
+}
+
 export default (obj, attr) => {
+
     console.log(obj);
+    initialize(obj);
     console.log(attr);
 }
